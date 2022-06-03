e079853e030076fec5d33ce30854d171c1a5df0b
mbastidasluis <luis.bastidas@previsora.com>
2022-06-03 17:42:38 -0400

    * Added TailwindCSS. * Added new nav component.

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,7 @@
 import { h } from 'preact';
-import style from './style.css';
-
 const Home = () => (
-  <div class="flex items-center bg-gray-100">
+  <div class="mt-4">
     <h1>Home</h1>
-    <p>This is the Home component using TailwindCSS.</p>
   </div>
 );
-
 export default Home;
