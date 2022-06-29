1f4030b0d7ab6c8512e2375ab437d3970389f924
mbastidasluis <luis.bastidas@previsora.com>
2022-06-28 21:52:21 -0400

    added configuration for development and deployment environment.

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,8 +3,21 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO add development enviromenment config
-let firebaseConfig = require('../pro.appConfig.json');
+// added configuration for development and deployment environme
+let firebaseConfig;
+if (process && process.env) {
+  firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId,
+  };
+} else {
+  firebaseConfig = require('../pro.appConfig.json');
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
