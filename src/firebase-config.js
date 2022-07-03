3887e6915f9bdfb156eb4e752bfb577f5e60aa44
mbastidasluis <luis.bastidas@previsora.com>
2022-07-03 17:00:38 -0400

    Simplified firebase init config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,29 +2,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// force save env variables to process env
-require('dotenv').config();
-
 // added configuration for development and deployment environme
-let firebaseConfig;
-
-if (process && process.env) {
-  const req = require('../pro.appConfig.json');
-  console.log('req', req);
-  console.log('process', process);
-
-  firebaseConfig = {
-    apiKey: process.env.PREACT_APP_APIKEY,
-    authDomain: process.env.PREACT_APP_AUTHDOMAIN,
-    projectId: process.env.PREACT_APP_PROJECTID,
-    storageBucket: process.env.PREACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.PREACT_APP_MESSAGINGSENDERID,
-    appId: process.env.PREACT_APP_APPID,
-    measurementId: process.env.PREACT_APP_MEASUREMENTID,
-  };
-} else {
-  firebaseConfig = require('../pro.appConfig.json');
-}
+let firebaseConfig = require('../pro.appConfig.json');
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
